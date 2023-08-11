9e0f0bbec945b5cedfd9d34aa820881b980636a7
luisantoniocrag <tricubeart@gmail.com>
2023-08-10 23:39:38 -0600

    add base of header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link"
 import { signIn, signOut, useSession } from "next-auth/react"
 import styles from "./header.module.css"
+import { ArrowRightOnRectangleIcon } from "@heroicons/react/20/solid"
 
 // The approach used in this component shows how to build a sign in and sign out
 // component that works on pages which support both client and server side
@@ -9,87 +10,23 @@ export default function Header() {
   const { data: session, status } = useSession()
   const loading = status === "loading"
 
+
+  const onLogin = () => {
+    signIn()
+  }
+
   return (
-    <header>
-      <noscript>
-        <style>{`.nojs-show { opacity: 1; top: 0; }`}</style>
-      </noscript>
-      <div className={styles.signedInStatus}>
-        <p
-          className={`nojs-show ${
-            !session && loading ? styles.loading : styles.loaded
-          }`}
-        >
-          {!session && (
-            <>
-              <span className={styles.notSignedInText}>
-                You are not signed in
-              </span>
-              <a
-                href={`/api/auth/signin`}
-                className={styles.buttonPrimary}
-                onClick={(e) => {
-                  e.preventDefault()
-                  signIn("worldcoin") // when worldcoin is the only provider
-                  // signIn() // when there are multiple providers
-                }}
-              >
-                Sign in
-              </a>
-            </>
-          )}
-          {session?.user && (
-            <>
-              {session.user.image && (
-                <span
-                  style={{ backgroundImage: `url('${session.user.image}')` }}
-                  className={styles.avatar}
-                />
-              )}
-              <span className={styles.signedInText}>
-                <small>Signed in as</small>
-                <br />
-                <strong>{session.user.email ?? session.user.name}</strong>
-              </span>
-              <a
-                href={`/api/auth/signout`}
-                className={styles.button}
-                onClick={(e) => {
-                  e.preventDefault()
-                  signOut()
-                }}
-              >
-                Sign out
-              </a>
-            </>
-          )}
-        </p>
+    <header className="w-screen flex justify-between px-8 py-4">
+      <div id="title">
+        <h3>Ticketcat</h3>
       </div>
-      <nav>
-        <ul className={styles.navItems}>
-          <li className={styles.navItem}>
-            <Link href="/">Home</Link>
-          </li>
-          <li className={styles.navItem}>
-            <Link href="/client">Client</Link>
-          </li>
-          <li className={styles.navItem}>
-            <Link href="/server">Server</Link>
-          </li>
-          <li className={styles.navItem}>
-            <Link href="/protected">Protected</Link>
-          </li>
-          <li className={styles.navItem}>
-            <Link href="/api-example">API</Link>
-          </li>
-          <li className={styles.navItem}>
-            <Link href="/admin">Admin</Link>
-          </li>
-          <li className={styles.navItem}>
-            <Link href="/me">Me</Link>
-          </li>
-        </ul>
-      </nav>
+      <div>
+        <div
+          onClick={onLogin} 
+          className="p-2 rounded-full border border-slate-200 text-slate-600 flex items-center justify-center cursor-pointer hover:bg-slate-400 hover:text-white active:scale-110">
+            <ArrowRightOnRectangleIcon className="w-5 h-5 text-inherit" />
+        </div>
+      </div>
     </header>
   )
 }
