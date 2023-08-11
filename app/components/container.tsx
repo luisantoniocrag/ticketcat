04cba129d9941850f00dce6016a61bbf1ee255b8
luisantoniocrag <tricubeart@gmail.com>
2023-08-10 21:29:50 -0600

    fix container

--- app/components/container.tsx
+++ app/components/container.tsx
@@ -2,7 +2,7 @@ import Header from "./header"
 import Footer from "./footer"
 import type { ReactNode } from "react"
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default function Container({ children }: { children: ReactNode }) {
   return (
     <>
       <Header />
