137c3bb741deb4dd7bde62eb82bf62029638ae4e
luisantoniocrag <tricubeart@gmail.com>
2023-08-11 16:46:03 -0600

    add loading btn

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,14 +2,18 @@ import Link from "next/link"
 import { signIn, signOut, useSession } from "next-auth/react"
 import { ArrowRightOnRectangleIcon } from "@heroicons/react/20/solid"
 import { TicketcatLogoHorizontal } from "./svg"
+import { MoonLoader } from "react-spinners"
+import { useEffect } from "react"
 
-// The approach used in this component shows how to build a sign in and sign out
-// component that works on pages which support both client and server side
-// rendering, and avoids any flash incorrect content on initial page load.
+
 export default function Header() {
   const { data: session, status } = useSession()
-  const loading = status === "loading"
 
+  useEffect(() => {
+    console.log(session)
+  }, [session])
+
+  const loading = status === "loading"
 
   const onLogin = () => {
     signIn()
@@ -25,11 +29,16 @@ export default function Header() {
         </div>
       </div>
       <div>
-        <div
-          onClick={onLogin} 
-          className="p-2 rounded-full border border-slate-200 text-slate-600 flex items-center justify-center cursor-pointer hover:bg-slate-400 hover:text-white active:scale-110">
-            <ArrowRightOnRectangleIcon className="w-5 h-5 text-inherit" />
-        </div>
+      <button
+                onClick={onLogin} 
+
+        type="button"
+        className="inline-flex items-center gap-x-1.5 rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      >
+        {!loading ? <ArrowRightOnRectangleIcon className="w-5 h-5 text-inherit" /> : <MoonLoader size={15} />}
+        Authenticate
+      </button>
+        
       </div>
     </header>
   )
